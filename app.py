a57b207da0df91d646cf7473e21779f1812e2e48
Jiayu Hu <jiayuhu@cmail.carleton.ca>
2023-01-14 22:36:02 -0500

    able to get data from app.py and deploy on the output.html

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from flask import request
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session
 app = Flask(__name__)
 from crawler import crawl
 from resume_pdf import get_resume
@@ -12,7 +12,11 @@ def index():
 
 @app.route('/output.html', methods=['GET', 'POST'])
 def output():
-    return render_template('output.html')
+    with open('job.txt', 'r') as file:
+        job = file.read()
+    with open('resume.txt', 'r') as file:
+        resume = file.read()
+    return render_template('output.html', job=job, resume=resume)
 
 
 @app.route('/input.html', methods=['GET', 'POST'])
@@ -20,11 +24,15 @@ def input():
     if request.method == "POST":
         output = request.get_json()
         result = json.loads(output)
-        url = crawl(result.get("job")).strip()
-        print(url)
+        job = crawl(result.get("job")).strip()
+        print(job)
+        with open('job.txt', 'w') as f:
+            f.write(job)
         resume = get_resume(result.get("resume"))
         print(resume)
-        return render_template('output.html', value = url)
+        with open('resume.txt', 'w') as f:
+            f.write(resume)
+        return render_template("output.html")
     else:
         return render_template("input.html")
 
